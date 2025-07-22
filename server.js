3ccebbd496f408e8dad34af4dd0970464dcafdbb
adarshaharshavardhana@gmail.com <adarshaharshavardhana@gmail.com>
2025-07-22 21:13:40 +0530

    roll back1

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import propertyRoutes from './routes/propertyRoute.js';
 import cors from 'cors';
 
 dotenv.config();
-
+PORT="https://gaurdan-backend-1.onrender.com"
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -17,6 +17,6 @@ mongoose.connect(process.env.MONGO_URI, {
   useUnifiedTopology: true,
 })
 .then(() => {
-  app.listen(process.env.PORT, () => console.log('Server running on port 5000'));
+  app.listen(PORT, () => console.log('Server running on port 5000'));
 })
 .catch((err) => console.error(err));
